da99ec226788bca859a0b1fcce4447f736a29c59
iteesha22 <jindalteesha22@gmail.com>
2025-12-11 13:37:53 +0530

    commited

--- data_types.py
+++ data_types.py
@@ -13,5 +13,12 @@ var3 = np.array(["A","b","D","e",3,4,9,6])
 print("Data Type: " ,var3.dtype)
 
 # To change dtype of an array:
-x=np.array([1,2,3,4],dtype=np.int8)
+
+x=np.array([1,2,3,4],)
 print(x.dtype)
+
+x1=np.array([1,2,3,4],dtype=np.int8)
+print(x1.dtype)
+
+y=np.array([1,2,3,4],dtype = "f")
+print(y.dtype)
