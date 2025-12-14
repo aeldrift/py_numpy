710fae2dc7328198bc3150891a2e999a7bf05cfa
iteesha22 <jindalteesha22@gmail.com>
2025-12-14 14:35:16 +0530

    commited

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+var = np.array([1,2,3,4])
+var_add = var+3
+print("After adding 3 to each element:",var_add)
+
+var1 = np.array([10,20,30,40])
+var_add1 = var + var1
+print("After adding two arrays element-wise:",var_add1)
