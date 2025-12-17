13acfbdae2f37a0cc34897ecb4cd1f1dac7b4c40
iteesha22 <jindalteesha22@gmail.com>
2025-12-17 18:04:53 +0530

    commited

--- iiteration.py
+++ iiteration.py
@@ -2,7 +2,17 @@
 # iteration:
 import numpy as np
 
-# x=[1,2,3,4,5]
-# print("printing using for loop:")
-# for i in x:
-#     print(i)
+x=[1,2,3,4,5]
+print("printing using for loop:")
+for i in x:
+    print(i)
+
+print("value of i with indexing:")
+for i in x[1:4]:
+    print(i)
+
+arr = np.array([10,20,30,40,50])
+print("arr is:",arr)
+
+for i in arr:
+    print(i)
