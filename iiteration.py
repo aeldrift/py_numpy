22f547fe9ae2d5cbbf648ede71abd68884077740
iteesha22 <jindalteesha22@gmail.com>
2025-12-17 18:06:33 +0530

    commited

--- iiteration.py
+++ iiteration.py
@@ -16,3 +16,46 @@ print("arr is:",arr)
 
 for i in arr:
     print(i)
+
+
+arr2d = np.array([[100,200,300],[400,500,600]])
+print("arr2d is:\n",arr2d)
+print()
+for j in arr2d:
+    print(j)
+
+# iteration in 2-D array:
+print("iteration in 2-D array:")
+for j in arr2d:
+    for k in j:
+        print(k)
+
+arr = np.array([[1,2,3,4],[5,6,7,8]],ndmin=7)
+print("dimensionof array arr is:",arr.ndim)
+
+for i in np.nditer(arr):
+    print(i)
+
+for i in np.nditer(arr,flags=['buffered'],op_dtypes=["S"]):
+    print(i)
+
+for i in np.nditer(arr):
+    print("i is:", i)
+
+# astype():
+new_arr = arr.astype("S")
+print("new_arr is:\n",new_arr)
+
+# readwrite():
+# example:1
+for i in np.nditer(arr,op_flags=['readwrite']):
+      i[...] = i * 1
+print("i is:",i)
+
+# example:2
+arr = np.array([1, 2, 3])
+
+for i in np.nditer(arr, op_flags=['readwrite']):
+    i[...] *= 10  # as 3 elements are there, so loop'll run 3 times
+
+print(arr)
